e298e9e6de532525b9238912b618a9e27b3111dc
Dexter432 <95933253+Dexter432@users.noreply.github.com>
2022-05-20 12:36:12 -0300

    version3

--- sketch.js
+++ sketch.js
@@ -1,15 +1,19 @@
 var canvas;
 var backgroundImage, car1_img, car2_img, track;
+var fuelImage, powerCoinImage;
+
 var database, gameState;
 var form, player, playerCount;
-var allPlayers, car1, car2;
+var allPlayers, car1, car2, fuels, powerCoins;
 var cars = [];
-
+var obstacles,obstacles1,obstacles2
 function preload() {
   backgroundImage = loadImage("assets/planodefundo.png");
   car1_img = loadImage("assets/car1.png");
   car2_img = loadImage("assets/car2.png");
   track = loadImage("assets/pista.png");
+  fuelImage = loadImage("assets/fuel.png");
+  powerCoinImage = loadImage("assets/goldCoin.png");
 }
 
 function setup() {
